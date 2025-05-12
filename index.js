671ffd666b08e2b31bdf6fa986fa742fce940552
Anurag <anuragsharmaroy@gmail.com>
2025-05-12 12:21:45 +0530

    Create index.js
    creating corrected file

---
+++ index.js
@@ -0,0 +1,19 @@
+var express=require('express')
+var bodyParser=require('body-parser')
+var mysql=require('mysql')
+var app=express()
+app.use(bodyParser,json())
+
+app.get("/",function(request,response){
+  response.send("hello from express js....")
+});
+
+
+app.get("/date",function(request,response){
+  response.send("Date is:.."+new Date().toLocaleString())
+});
+
+app.listen(9595,function(err){
+  if (err) throw err
+  console.log("Server started on port 9595, to shut down hit ctrl+c")
+})
