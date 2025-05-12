cd1db604ef3f89c17a689064929dd707ae6cbb06
Anurag <anuragsharmaroy@gmail.com>
2025-05-12 14:04:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.get("/date",function(request,response){
   response.send("Date is:.."+new Date().toLocaleString())
 });
 
-app.listen(9595,function(err){
+app.listen(9596,function(err){
   if (err) throw err
-  console.log("Server started on port 9595, to shut down hit ctrl+c")
+  console.log("Server started on port 9596, to shut down hit ctrl+c")
 })
